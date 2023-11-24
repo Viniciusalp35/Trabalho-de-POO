96e6e4cef03ce49cdebe56f4e4872a7343e6b03f
Vinicius lobo <joaobueno121212@gmail.com>
2023-11-24 16:00:04 -0300

    Imagem de fundo Vermont

--- LocadoraVermont/src/gui/Login.java
+++ LocadoraVermont/src/gui/Login.java
@@ -1,5 +1,7 @@
 package gui;
 
+import login.CadastroUsuarios;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,25 +13,39 @@ public class Login extends JFrame {
     public Login() throws Exception{
         super("Locadora Vermont");
 
+        ImageIcon imagem = new ImageIcon("src/login/VermontMenor.jpg");
+        JLabel backgroundLabel = new JLabel(imagem);
+        backgroundLabel.setBounds(0, 0, 800, 600);
+        add(backgroundLabel);
+
+        //getContentPane().setBackground(Color.cyan);
+
+
         login = new JButton("Login");
         cadastro = new JButton("Cadastro");
+        login.setBounds(50, 250, 100, 30);
+        login.setBackground(new Color(66, 141, 245));
+
+        cadastro.setBackground(new Color(66, 141, 245));
+        cadastro.setBounds(50, 300, 100, 30);
+        this.add(login);
+        this.add(cadastro);
 
         JPanel panel = new JPanel();
-        panel.add(login);
-        panel.add(cadastro);
-
+        panel.setOpaque(false);
         getContentPane().add(panel);
 
-        getContentPane().setBackground(Color.cyan);
-        panel.setOpaque(false);
-
-        setSize(500, 400);
+        setSize(800, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-
-
     }
 
+    private void exibirTelaLogin() {
+        //LoginDialog.showLoginDialog(this,cadastroUsuarios);
+    }
 
+    private void exibirTelaCadastro(CadastroUsuarios cadastroUsuarios) {
+        //RegisterDialog.showRegisterDialog(cadastroUsuarios);
+    }
 }
