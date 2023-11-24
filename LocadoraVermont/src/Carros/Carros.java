77af550530354402b267fef46397acb946b54f79
Viniciusalp35 <vinicius.alp5626@gmail.com>
2023-11-23 21:04:09 -0300

    classe carro, quase la

--- LocadoraVermont/src/Carros/Carros.java
+++ LocadoraVermont/src/Carros/Carros.java
@@ -1,4 +1,45 @@
 package Carros;
 
-public class Carros {
+public abstract class Carros {
+    private String modelo;
+    private String cor;
+    private double preço;
+    private boolean Status;
+    private String Tipo;
+
+
+    public Carros(boolean Status){
+        this.Status = Status;
+    }
+
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public double getPreço() {
+        return preço;
+    }
+    public abstract void setPreço(double preço);
+
+    public boolean isStatus() {
+        return Status;
+    }
+    public void setStatus(boolean status) {
+        this.Status = status;
+    }
+    public String getTipo() {
+        return Tipo;
+    }
+    public void setTipo(String tipo) {
+        Tipo = tipo;
+    }
 }
