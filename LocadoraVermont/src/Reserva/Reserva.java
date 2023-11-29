c2328ed0c61890c4f3d585376340e740e9646e72
Paulo Arthur <pauloarthur.ce@gmail.com>
2023-11-28 23:31:47 -0300

    implementando construtor

--- LocadoraVermont/src/Reserva/Reserva.java
+++ LocadoraVermont/src/Reserva/Reserva.java
@@ -1,18 +1,39 @@
 package Reserva;
 
 import Carros.Carros;
+import login.Usuarios;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Reserva {
 
     //atributos
     private Carros carro;
-    private String dataInicio;
-    private String dataFim;
+    private Usuarios usuario;
+    private Date dataInicio;
+    private Date dataFim;
     private double valorTotal;
 
     //métodos
 
-
+    public Reserva(Carros carro, Usuarios usuario, String dataInicio, String dataFim) {
+        this.carro = carro;
+        this.usuario = usuario;
+        try {
+            this.dataInicio = new SimpleDateFormat("dd/MM/yyyy").parse(dataInicio);
+        } catch (ParseException ex) {
+            Logger.getLogger(Reserva.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            this.dataFim = new SimpleDateFormat("dd/MM/yyyy").parse(dataInicio);
+        } catch (ParseException ex) {
+            Logger.getLogger(Reserva.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     public Carros getCarro() {
         return carro;
@@ -22,19 +43,19 @@ public class Reserva {
         this.carro = carro;
     }
 
-    public String getDataInicio() {
+    public Date getDataInicio() {
         return dataInicio;
     }
 
-    public void setDataInicio(String dataInicio) {
+    public void setDataInicio(Date dataInicio) {
         this.dataInicio = dataInicio;
     }
 
-    public String getDataFim() {
+    public Date getDataFim() {
         return dataFim;
     }
 
-    public void setDataFim(String dataFim) {
+    public void setDataFim(Date dataFim) {
         this.dataFim = dataFim;
     }
 
